87b9e0438c341e6094bbb4fa05d6198103d87c0c
Marcelo Dias <marcelocdias93@gmail.com>
2021-08-01 22:12:56 -0300

    Add bootstrap

---
+++ app/frontend/packs/application.js
@@ -0,0 +1,12 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import '../js/bootstrap_js_files.js'
+import "@fortawesome/fontawesome-free/js/all";
+
+require("jquery")
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
